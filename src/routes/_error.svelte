d446a5a3c2b2be78f709d482344af7011f0c767a
Mauro Garcia <maurogarcia0209@gmail.com>
2019-12-08 13:07:59 -0300

    error page improvements

--- src/routes/_error.svelte
+++ src/routes/_error.svelte
@@ -31,10 +31,13 @@
 	<title>{status}</title>
 </svelte:head>
 
-<h1>{status}</h1>
-
-<p>{error.message}</p>
-
-{#if dev && error.stack}
-	<pre>{error.stack}</pre>
-{/if}
+<div
+    class="flex flex-col h-screen justify-center items-center text-white w-1/2
+    mx-auto">
+	<span class="text-6xl"><i class="fas fa-exclamation-triangle mb-4" /> {status}</span>
+    
+    <p class="text-2xl text-center mb-4">
+      {error.message}
+    </p>
+    <a class="text-blue-500" href="/">Back to home</a>
+  </div>
