2a328568c6195ca67883c6d9373a45286082363e
Mauro Garcia <maurogarcia0209@gmail.com>
2019-12-08 12:31:08 -0300

    updated meta tags and favicon

--- src/components/FeaturedTags.svelte
+++ src/components/FeaturedTags.svelte
@@ -10,7 +10,7 @@
 
 <div
   class="bg-gray-800 w-full p-8 rounded-lg mr-4 items-center justify-center body">
-  <h1 class="text-xl mb-4">MOST USED TAGS</h1>
+  <h1 class="text-xl mb-4">YOUR FAVORITE TAGS</h1>
   <div class="flex flex-wrap">
     {#each mostUsedTags as tag}
       <p class="bg-teal-500 px-4 py-2 rounded mr-2 mb-2 text-xl">#{tag}</p>
